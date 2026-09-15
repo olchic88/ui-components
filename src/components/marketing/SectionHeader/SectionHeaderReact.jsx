42903afc058f78a0e2a913bdd807d8fac674c753
Olga Romanova <olgaromanova@MacBook-Pro-Olga.local>
2026-09-15 16:39:23 +0200

    refactor: make SectionHeaderReact styles configurable

--- src/components/marketing/SectionHeader/SectionHeaderReact.jsx
+++ src/components/marketing/SectionHeader/SectionHeaderReact.jsx
@@ -5,14 +5,16 @@ export default function SectionHeaderReact({
   title,
   subtitle,
   heading = "h2",
+  className = "",
+  supportingTextClassName = "",
   titleClassName = "",
   subtitleClassName = "",
 }) {
   const Heading = heading; /* for dynamic heading */
 
   return (
-    <div className="section-header">
-      <p className="section-header-supporting-text">{supportingText}</p>
+    <div className={`section-header ${className}`}>
+      <p className={`section-header-supporting-text ${supportingTextClassName}`}>{supportingText}</p>
       <div className="section-header-content">
         <Heading className={`section-header-title ${titleClassName}`}>
           {title}
