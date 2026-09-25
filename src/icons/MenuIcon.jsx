f8dab6270c3c8d1a92b3b9f2b4e580fbf74321c0
Olga Romanova <olgaromanova@MacBook-Pro-Olga.local>
2026-09-25 12:11:00 +0200

    style: improve Navbar styles

--- src/icons/MenuIcon.jsx
+++ src/icons/MenuIcon.jsx
@@ -9,7 +9,7 @@ export function MenuIcon({ size = 24 }) {
     >
       <path
         d="M2.5 3.33333H17.5V5H2.5V3.33333ZM2.5 9.16667H17.5V10.8333H2.5V9.16667ZM2.5 15H17.5V16.6667H2.5V15Z"
-        fill="#525252"
+        fill="currentColor"
       />
     </svg>
   );
